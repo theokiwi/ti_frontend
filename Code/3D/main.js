b06b90885f3ed50185c0d5a8f495fcaeac7d4f20
Felipe Maia <felipe.maia2113@gmail.com>
2024-05-19 16:11:11 -0300

    patch

--- Code/3D/main.js
+++ Code/3D/main.js
@@ -2,12 +2,12 @@ import * as THREE from 'three'
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import {ArcballControls} from 'three/addons/controls/ArcballControls.js'
 
-const FOV = 75;
-const CAMERA_DEPTH = 5;
-const W_WIDTH = window.innerWidth;
-const W_HEIGHT = window.innerHeight;
+const FOV = 45;
+const W_WIDTH = 800;
+const W_HEIGHT = 600;
 const PIXEL_RATIO = window.devicePixelRatio;
 const ASPECT_RATIO = W_WIDTH / W_HEIGHT;
+const MAIN_CANVAS = document.querySelector("#main-canvas");
 
 //COLORS
 const C_BLACK = 0x000000;
@@ -17,17 +17,19 @@ const C_WHITE = 0xffffff;
 const scene = new THREE.Scene();
 
 //RENDERER
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    canvas: MAIN_CANVAS,
+    alpha: true
+});
 renderer.setSize(W_WIDTH, W_HEIGHT);
 renderer.setPixelRatio(PIXEL_RATIO);
-renderer.setClearColor(C_BLACK);
+renderer.setClearColor(C_BLACK, 0);
 renderer.outputColorSpace = THREE.SRGBColorSpace;
-document.body.appendChild(renderer.domElement);
 
 //CAMERA
 const camera = new THREE.PerspectiveCamera(FOV, ASPECT_RATIO, 1 , 1000);
 camera.position.set(0, 5, 10);
-camera.lookAt(0,0,0);
+camera.lookAt(0, 0, 0);
 
 //SPOTLIGHT
 const spotLight = new THREE.SpotLight(C_WHITE, 3000, 100, 0.22, 1);
