165c76414deb7c9a7c82b300d8a88c06ffbe75e9
Mateus Ottoni <mateus.resende.ottoni@gmail.com>
2024-06-06 20:18:38 -0300

    Major: ForumPost made functional
    Added the javascript properties correctly, so now aswering the forum posts is possible and it can load different data from the json

--- Code/Forum/ForumPost/assets/scripts/app.js
+++ Code/Forum/ForumPost/assets/scripts/app.js
@@ -1,64 +1,235 @@
 function read_forumdata() {
-    let strDados = localStorage.getItem('db');
-    let objDados = {};
+    let strDados = localStorage.getItem('ArtsyForumPosts');
+    let Dados = {};
 
     if (strDados) {
-        objDados = JSON.parse(strDados);
+        Dados = JSON.parse(strDados);
     }
     else {
-        objDados = {
+        Dados = {
 
-            
-            "pergunta": {
-                "usuario": "Pablo Picasso", foto: "assets/images/perfil.png",
-                "respostas": "01", views: "03", likes: "01", dislikes: "02",
-                "tag": "Tag 1", titulo: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
-                "pergunta": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis, aperiam commodi quibusdam officiis officia unde, corrupti corporis esse magnam, natus sunt! Excepturi porro eius dolore sequi corrupti aliquid ipsa saepe dolor ad? Earum neque minus iusto numquam illo tempora iste ab blanditiis doloribus eaque, odit illum adipisci nobis amet, rem quis. Temporibus odio esse qui quae iste vero unde voluptatibus iure excepturi, ipsum accusantium velit eius totam nobis, non obcaecati aperiam maiores at, tenetur deserunt molestias! Impedit earum veniam quaerat, sint voluptas et consequuntur magni dolores pariatur vero animi fugit assumenda sunt esse adipisci aspernatur cumque blanditiis?"
-            },
-            "respostas": [
+            "forum": [
                 {
-                    "usuario": "Mauricio de Sousa", foto: "assets/images/perfil.png",
-                    "likes": "01", dislikes: "00",
-                    "resposta": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint perferendis commodi aspernatur repudiandae suscipit quis incidunt. Ad sed doloribus officiis sunt quae. Dignissimos earum libero id nobis nostrum minus, aperiam debitis voluptate pariatur minima accusantium sint enim saepe reiciendis incidunt modi consequatur ut."
+                    "post": {
+                        "usuario": "Pablo Picasso", "forumID": 1,
+                        "respostas": 1, "views": 3, "likes": 7, "dislikes": 1,
+                        "tag": "Tag 1", "titulo": "Primeiro Lorem ipsum, dolor sit amet consectetur adipisicing.",
+                        "pergunta": "1Adipisci aspernatur cumque blanditiis?",
+                    },
+                    "respostas": [
+                        {
+                            "usuario": "Mauricio de Sousa",
+                            "likes": "01", "dislikes": "00",
+                            "resposta": "Lorem ai1 ipsum dolor sit amet consectetur, adipisicing elit. Ad sed doloribus officiis sunt quae. Dignissimos earum libero id nobis nostrum minus, aperiam debitis voluptate pariatur minima accusantium sint enim saepe reiciendis incidunt modi consequatur ut."
+                        },
+                        {
+                            "usuario": "Mauricio de Sousa",
+                            "likes": "02", "dislikes": "00",
+                            "resposta": "Lorem ai2 ipsum dolor sit amet consectetur, adipisicing elit. Ad sed doloribus officiis sunt quae. Dignissimos earum libero id nobis nostrum minus, aperiam debitis voluptate pariatur minima accusantium sint enim saepe reiciendis incidunt modi consequatur ut."
+                        }]
+                },
+                {
+                    "post": {
+                        "usuario": "Pablo Picasso", "forumID": 2,
+                        "respostas": 2, "views": 4, "likes": 2, "dislikes": 2,
+                        "tag": "Tag 2", "titulo": "Segundo Lorem ipsum, dolor sit amet consectetur adipisicing.",
+                        "pergunta": "2Adipisci aspernatur cumque blanditiis?"
+                    },
+                    "respostas": [
+                        {
+                            "usuario": "Mauricio de Sousa",
+                            "likes": "01", "dislikes": "00",
+                            "resposta": "Lorem ei ipsum dolor sit amet consectetur, adipisicing elit. Ad sed doloribus officiis sunt quae. Dignissimos earum libero id nobis nostrum minus, aperiam debitis voluptate pariatur minima accusantium sint enim saepe reiciendis incidunt modi consequatur ut."
+                        }]
+                },
+                {
+                    "post": {
+                        "usuario": "Pablo Picasso", "forumID": 3,
+                        "respostas": 3, "views": 5, "likes": 6, "dislikes": 1,
+                        "tag": "Tag 3", "titulo": "Terceiro Lorem ipsum, dolor sit amet consectetur adipisicing.",
+                        "pergunta": "3Adipisci aspernatur cumque blanditiis?"
+                    },
+                    "respostas": [
+                        {
+                            "usuario": "Mauricio de Sousa",
+                            "likes": "01", "dislikes": "00",
+                            "resposta": "Lorem ii ipsum dolor sit amet consectetur, adipisicing elit. Ad sed doloribus officiis sunt quae. Dignissimos earum libero id nobis nostrum minus, aperiam debitis voluptate pariatur minima accusantium sint enim saepe reiciendis incidunt modi consequatur ut."
+                        }]
+                },
+                {
+                    "post": {
+                        "usuario": "Pablo Picasso", "forumID": 4,
+                        "respostas": 4, "views": 6, "likes": 3, "dislikes": 2,
+                        "tag": "Tag 4", "titulo": "Quarto Lorem ipsum, dolor sit amet consectetur adipisicing.",
+                        "pergunta": "4Adipisci aspernatur cumque blanditiis?"
+                    },
+                    "respostas": [
+                        {
+                            "usuario": "Mauricio de Sousa",
+                            "likes": "01", "dislikes": "00",
+                            "resposta": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad sed doloribus officiis sunt quae. Dignissimos earum libero id nobis nostrum minus, aperiam debitis voluptate pariatur minima accusantium sint enim saepe reiciendis incidunt modi consequatur ut."
+                        }]
                 }
             ]
         }
     }
 
-    return objDados;
+    return Dados;
 }
 
 function insert_forum() {
 
-    let objDados = read_forumdata();
+    let Dados = read_forumdata();
 
-    $(".question_tag").text(objDados.pergunta.tag);
-    $(".question_title").text(objDados.pergunta.titulo);
-    $(".post_replies_number").text(objDados.pergunta.respostas);
-    $(".post_views_number").text(objDados.pergunta.views);
-    $(".post_likes_number").text(objDados.pergunta.likes);
-    $(".post_dislikes_number").text(objDados.pergunta.dislikes);
-    $(".post_text").text(objDados.pergunta.pergunta);
-    $(".post_owner").attr("src", objDados.pergunta.foto);
+    let getquestionID = new URLSearchParams(location.search);
+    let questionID = getquestionID.get('forumID');
+    questionID = 1;
 
 
-        for (x=0; x < objDados.respostas.length; x++) {
-            $(".answer_likes_number").text(objDados.respostas[x].likes);
-            $(".answer_dislikes_number").text(objDados.respostas[x].dislikes);
-            $(".answer_text").text(objDados.respostas[x].resposta);
-            $(".answer_owner").attr("src",objDados.respostas[x].foto);
-        }
+    $(".post_answers").append(' <div class="post"> \
+    <div class="post_data"> \
+        <img src="assets/images/perfil.png" alt="Autor" class="post_owner"> \
+        <div class="post_replies"> \
+            <i class="post_replies_icon fa-solid fa-reply"></i> \
+            <span class="post_replies_number">01</span> \
+        </div> \
+        <div class="post_views"> \
+            <i class="post_views_icon fa-solid fa-eye"></i> \
+            <span class="post_views_number">03</span> \
+        </div> \
+    </div> \
+\
+    <div class="post_text"> \
+        <p>Pergunta Placeholder</p> \
+    </div> \
+\
+    <div class="post_rating"> \
+        <div class="post_likes"> \
+            <button><i class="post_likes_icon fa-solid fa-angle-up"></i></button> \
+            <span class="post_likes_number">01</span> \
+        </div> \
+        <div class="post_dislikes"> \
+            <button><i class="post_dislikes_icon fa-solid fa-angle-down"></i></button> \
+             <span class="post_dislikes_number">02</span> \
+         </div> \
+    </div> ');
+
+    // currentquestion = forum.find (function (Elemento) {Elemento.forumID == questionID})
+    $(".question_tag").text(Dados.forum[questionID - 1].post.tag);
+    $(".question_title").text(Dados.forum[questionID - 1].post.titulo);
+    $(".post_replies_number").text(Dados.forum[questionID - 1].post.respostas);
+    $(".post_views_number").text(Dados.forum[questionID - 1].post.views);
+    $(".post_likes_number").text(Dados.forum[questionID - 1].post.likes);
+    $(".post_dislikes_number").text(Dados.forum[questionID - 1].post.dislikes);
+    $(".post_text").text(Dados.forum[questionID - 1].post.pergunta);
+    //    $(".post_owner").attr("src", Dados.forum[questionID - 1].post.foto);
+
+
+    for (x = 0; x < Dados.forum[questionID - 1].respostas.length; x++) {
+        $(".post_answers").append('<div class="answer"> \
+        <div class="answer_data"> \
+        <img src="assets/images/perfil.png" alt="Respondedor" class="answer_owner"> </div> \
+        <div class="answer_text"> \
+        <p>Placeholder</p> </div> \
+        <div class="answer_rating"> \
+        <div class="answer_likes"> \
+        <button><i class="answer_likes_icon fa-solid fa-angle-up"> \
+        </i></button> <span class="answer_likes_number">10</span> </div>\
+        <div class="answer_dislikes"> \
+        <button><i class="answer_dislikes_icon fa-solid fa-angle-down"> \
+        </i></button> <span class="answer_dislikes_number">0</span> </div>\
+        \ </div> ');
+    }
+
+    for (x = 0; x < Dados.forum[questionID - 1].respostas.length; x++) {
+        $(".answer_likes_number").eq(x).text(Dados.forum[questionID - 1].respostas[x].likes);
+        $(".answer_dislikes_number").eq(x).text(Dados.forum[questionID - 1].respostas[x].dislikes);
+        $(".answer_text").eq(x).text(Dados.forum[questionID - 1].respostas[x].resposta);
+        // $(".answer_owner").eq(x).attr("src", Dados.forum[questionID - 1].respostas[x].foto);
+    }
 
 }
 
-function save_forum() {
+function update_forum() {
 
+    let Dados = read_forumdata();
+
+    let getquestionID = new URLSearchParams(location.search);
+    let questionID = getquestionID.get('forumID');
+    questionID = 1;
+
+    let tamanho = Dados.forum[questionID - 1].respostas.length;
+
+    $(".post_answers").append('<div class="answer"> \
+        <div class="answer_data"> \
+        <img src="assets/images/perfil.png" alt="Respondedor" class="answer_owner"> </div> \
+        <div class="answer_text"> \
+        <p>Placeholder</p> </div> \
+        <div class="answer_rating"> \
+        <div class="answer_likes"> \
+        <button><i class="answer_likes_icon fa-solid fa-angle-up"> \
+        </i></button> <span class="answer_likes_number">10</span> </div>\
+        <div class="answer_dislikes"> \
+        <button><i class="answer_dislikes_icon fa-solid fa-angle-down"> \
+        </i></button> <span class="answer_dislikes_number">0</span> </div>\
+        \ </div> ');
+
+    $(".answer_likes_number").eq(tamanho - 1).text(Dados.forum[questionID - 1].respostas[tamanho - 1].likes);
+    $(".answer_dislikes_number").eq(tamanho - 1).text(Dados.forum[questionID - 1].respostas[tamanho - 1].dislikes);
+    $(".answer_text").eq(tamanho - 1).text(Dados.forum[questionID - 1].respostas[tamanho - 1].resposta);
+    // $(".answer_owner").eq(tamanho).attr("src", Dados.forum[questionID - 1].respostas[tamanho].foto);
 }
+
+function save_answer() {
+    let Dados = read_forumdata();
+
+    let getquestionID = new URLSearchParams(location.search);
+    let questionID = getquestionID.get('forumID');
+    questionID = 1;
+
+    let strResposta = $("#new_answer").val();
+
+    if (strResposta == "") {
+        alert("Erro de envio: Preencha o campo de informação.");
+    }
+    else {
+        // Incluir informações novas
+        let novaResposta = {
+            "usuario": "Usuario",
+            "likes": 0, "dislikes": 0,
+            "resposta": strResposta
+        };
+        Dados.forum[questionID - 1].respostas.push(novaResposta);
+
+        save_forum(Dados);
+    }
+
+    return (Dados);
+}
+
+function save_forum(Dados) {
+    // Salvar os dados no localStorage
+    localStorage.setItem('ArtsyForumPosts', JSON.stringify(Dados));
+    //console.log(JSON.stringify(Dados));
+
+    alert("Sua pergunta foi salva");
+}
+
 
 $(document).ready(function () {
 
+    //localStorage.clear();
+
     insert_forum();
 
+    $('#submit_answer').click(function () {
+        let DadosComparacao = read_forumdata();
+        let DadosObtidos = read_forumdata();
 
+        DadosObtidos = save_answer();
+        if (DadosObtidos != DadosComparacao) {
+            update_forum();
+        }
+    });
 
 })
