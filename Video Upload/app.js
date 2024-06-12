0bb1cda48468edf0f1e39357f5b694b7fc221f79
Felipe Maia <felipe.maia2113@gmail.com>
2024-06-12 15:37:13 -0300

    video upload

---
+++ Video Upload/app.js
@@ -0,0 +1,22 @@
+const input = document.querySelector("#input-file");
+input.addEventListener("change", (event) => {
+    const input_target = event.target;
+    const video = input_target.files[0];
+    if(video){
+        const reader = new FileReader();
+        reader.addEventListener("load", (event) => {
+            let div = document.createElement("div");
+            div.innerHTML = `
+                <video
+                    autoplay
+                    controls 
+                    width="640" 
+                    height="480" 
+                    src="${reader.result}"
+                >
+            `;
+            document.body.append(div);
+        })
+        reader.readAsDataURL(video);
+    }
+});
