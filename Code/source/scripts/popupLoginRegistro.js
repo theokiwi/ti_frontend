1183988d5cc04a1bd9bb4fb8b5e535d46ca966aa
ale-andrade-o <166027426+ale-andrade-o@users.noreply.github.com>
2024-06-13 14:02:58 -0300

    Add files via upload
    Arquivo JS PopUp Login e Registro

---
+++ Code/source/scripts/popupLoginRegistro.js
@@ -0,0 +1,35 @@
+let login= document.getElementById("buttonLogin")
+let poplogin= document.getElementById("poplogin")
+let btnClose= document.getElementById("btn-close")
+let registro= document.getElementById("buttonCriar")
+let popRegistro= document.getElementById("popRegistro")
+let btnCloseRegistro= document.getElementById("btn-close-registro")
+ 
+
+
+
+/* Pop Login*/
+
+
+
+login.onclick=function (){
+    poplogin.showModal()
+
+}
+
+btnClose.onclick=function(){
+
+    poplogin.close()
+}
+ 
+
+// Pop Registro
+registro.onclick= function (){
+    popRegistro.showModal()
+
+}
+
+btnCloseRegistro.onclick=function(){
+    popRegistro.close()
+}
+
