0941d4bf76dc824b86972f94b54b96105debc63c
Mateus Ottoni <mateus.resende.ottoni@gmail.com>
2024-06-28 20:01:35 -0300

    Minor: Updated Explorar portfolio display
    Now the amount displayed varies according to the JSON data. Also, it randomly chooses an image from the stash to be displayed

--- Code/source/scripts/port_show.js
+++ Code/source/scripts/port_show.js
@@ -79,14 +79,14 @@ function read_port_data(callback) {
 function insert_port() {
 
     read_explorar_data(function (Dados_Explore) {
-        read_port_data(function (Dados_Port) {
 
-            for (let x = 0; x < Dados_Port.length; x++) {
 
-                // Inserir port na página
-                $('.portfolio_showcase div').append('\
-                <div class="card mb-3">\
-                    <div class="row g-0">\
+        for (let x = 0; x < Dados_Explore.length; x++) {
+
+            // Inserir port na página
+            $('.row').eq(0).append('\
+                    <div class="card mb-3">\
+                <div class= "row g-0" >\
                         <div class="col-md-9">\
                             <a href="./visualizar.html"><img src="./assets/img/img1.png" class="img-fluid rounded-start"\
                                     alt="..."></a>\
@@ -98,7 +98,7 @@ function insert_port() {
                                     <div class="port_likes">\
                                         <label class="reaction_icon">\
                                             <input type="checkbox" class="likes_checkbox">\
-                                            <button><i class="fa-solid fa-thumbs-up"></i></button>\
+                                            <button value="0"><i class="fa-solid fa-thumbs-up"></i></button>\
                                         </label>\
                                         <span class="port_likes_number">0</span>\
                                     </div>\
@@ -115,23 +115,76 @@ function insert_port() {
                     </div>\
                 </div>');
 
-                // Mudar link para sua versão no visualizar
-                $('.col-md-9 a').eq(x).attr('href', './visualizar.html?id='
-                    .concat(Dados_Port[x].id).concat('?userId=').concat(Dados_Port[x].userId));
+            
+        }
+
+    });
+}
+
+function valorInicial() {
+    read_explorar_data(function (Dados_Explore) {
+
+        //console.log ("Expo init", Dados_Explore);
+
+        for (let x = 0; x < Dados_Explore.length; x = x + 1) {
+
+            // Mudar link para sua versão no visualizar
+            $('.col-md-9 a').eq(x).attr('href', './visualizar.html?id='
+                .concat(Dados_Explore[x].id).concat('?userId=').concat(Dados_Explore[x].userId));
+
+
+            // Mudar imagem para uma aleatória
+            var number = 1 + Math.floor(Math.random() * 6);
+            if (number == 1) { $('.col-md-9 a img').eq(x).attr('src', './assets/img/img1.png'); }
+            if (number == 2) { $('.col-md-9 a img').eq(x).attr('src', './assets/img/img2.png'); }
+            if (number == 3) { $('.col-md-9 a img').eq(x).attr('src', './assets/img/img5.png'); }
+            if (number == 4) { $('.col-md-9 a img').eq(x).attr('src', './assets/img/PortfolioPreview.png'); }
+            if (number == 5) { $('.col-md-9 a img').eq(x).attr('src', './assets/img/Port_Explo1.jpg'); }
+            if (number == 6) { $('.col-md-9 a img').eq(x).attr('src', './assets/img/Port_Explo2.jpg'); }
+
+
+            // Mudar nome do usuário
+            $('.user_name').eq(x).text(Dados_Explore[x].username);
+
+            // Adicionar valores de like e dislike
+            $('.port_likes_number').eq(x).text(Dados_Explore[x].likes)
+            $('.port_dislikes_number').eq(x).text(Dados_Explore[x].dislikes)
+
+        }
+
+    });
+}
+
+function insert_new_data() {
+    read_explorar_data(function (Dados_Explore) {
+        read_port_data(function (Dados_Port) {
+
+            console.log("Port new", Dados_Port);
+            console.log("Expo new", Dados_Explore);
+
+            for (let x = 0; x < Dados_Port.length; x = x + 1) {
 
                 // Conferir se há ou não um respectivo par nos dados do Explorar
                 let user_found = false; let id_found = false; let y = 0;
+                // Esse é o sistema de busca e confirmação mais correto. Porém tá dando bug, então vai o mais simples
+                /* 
                 while ((!user_found || !id_found) && y < Dados_Explore.length) {
                     user_found = false; id_found = false;
                     if (Dados_Explore[y].id == Dados_Port[x].id) { id_found = true; }
                     if (Dados_Explore[y].userId == Dados_Port[x].userId) { user_found = true; }
                     y++;
+                }   */
+                while (!id_found && y < Dados_Explore.length) {
+                    if (Dados_Explore[y].id == Dados_Port[x].id) { id_found = true; }
+                    y++;
                 }
+
                 // Se não houver, adicionar
                 if (y == Dados_Explore.length) {
                     let new_port = {
                         "id": Dados_Port[x].id,
                         "userId": Dados_Port[x].userId,
+                        "username": Dados_Port[x].whoAmI,
                         "likes": 0, "dislikes": 0
                     }
 
@@ -142,37 +195,18 @@ function insert_port() {
                     // Nothing, I guess
                 }
 
-                // Adicionar valores de like e dislike
-                /*
-                $(Insira aqui o local onde estão os likes).text(Dados_Explore[y].likes)
-                $(Insira aqui o local onde estão os dislikes).text(Dados_Explore[y].dislikes)
-                */
             }
 
-
-
+            console.log("Expo new after", Dados_Explore);
             save_explorar_data(Dados_Explore);
+
         });
     });
 }
-const valoriniciallikes="http://localhost:3000/explorar";
-function valorInicial(){
-    fetch(valoriniciallikes)
-    .then (res=>res.json())
-    .then(dados=>{
-        console.log(dados);
-        document.getElementById('plikes').innerText=dados[0].likes
-        document.getElementById('pdislikes').innerText=dados[0].dislikes
-    })
-}
-/*const updatelikes="http://localhost:3000/explorar";
-function update(){
-    fetch(updatelikes)
-    .then (res=>res.json())
-    .then(dados=>{
-        let valor=dados[0].likes
-    })
-}*/
+
+
+
+
 function save_explorar_data(Dados) {
     // Salvar os dados no localStorage
     localStorage.setItem('ArtsyExplorar', JSON.stringify(Dados));
@@ -180,10 +214,44 @@ function save_explorar_data(Dados) {
 
 $(document).ready(function () {
 
-    //insert_port();
+    localStorage.clear()
+
+
+
+    insert_new_data();
+    insert_port();
     valorInicial();
+
+
+
+
+    // Like
+    $('portfolio_showcase').on('click', '.row .card .row .col-md-3 .card-body .port_rating .port_likes label button', function () {
+        
+        // If para caso já esteja pressionado essa
+          // Se sim
+            // Desmarca
+            // Atualiza número e ícone
+          // Se não
+            // Caso o outro esteja selecionado
+              // Se sim
+                // Desmarca o outro e atualiza seu numero e icone
+                // Marca esse e atualiza seu numero e icone
+              // Se não
+                // Selecionar a checkbox e marcar ela
+                // Atualizar o número e o ícone (cor)
+
+    });
+    // Dislike
     $('portfolio_showcase').on('click', '.row .card .row .col-md-3 .card-body .port_rating .port_likes label button', function () {
 
-    })
- 
+    });
+
+
+
+
+    $('#btnordenar').click(function () {
+
+    });
+
 })
