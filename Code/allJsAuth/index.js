7cc95de552116cd7670264ae63b2985b07e9c94f
Theo <theodordiniz@gmail.com>
2024-07-03 14:02:15 -0300

    FINAL: Getting Ready for Deploy
    Tentando um novo script.

--- Code/allJsAuth/index.js
+++ Code/allJsAuth/index.js
@@ -1,10 +1,30 @@
-const jsonServer = require("json-server"); 
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; 
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
 
-server.use(middlewares);
-server.use(router);
+const server = jsonServer.create()
 
-server.listen(port);
+// Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
+
+module.exports = server
